8c006795aa668f8b67c43bb18bfbf099e9c9cdde
Kaido Kert <kaidokert@gmail.com>
2025-02-14 21:05:41 -0800

    Missing sub op

--- src/uint/sub.rs
+++ src/uint/sub.rs
@@ -59,6 +59,13 @@ impl<const LIMBS: usize> Sub<&Uint<LIMBS>> for Uint<LIMBS> {
             .expect("attempted to subtract with underflow")
     }
 }
+impl<const LIMBS: usize> Sub<Uint<LIMBS>> for &Uint<LIMBS> {
+    type Output = Uint<LIMBS>;
+
+    fn sub(self, rhs: Uint<LIMBS>) -> Self::Output {
+        (*self).sub(&rhs)
+    }
+}
 
 impl<const LIMBS: usize> SubAssign<Uint<LIMBS>> for Uint<LIMBS> {
     fn sub_assign(&mut self, rhs: Uint<LIMBS>) {
